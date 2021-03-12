94c79f5636522e6ecf7006d3ae721351e467ccbd
rongc <qiurongc@foxmail.com>
2021-03-12 18:57:23 +0800

    依赖库版本升级

--- src/main/groovy/com/rongc/plugin/config/Publishing.groovy
+++ src/main/groovy/com/rongc/plugin/config/Publishing.groovy
@@ -119,7 +119,7 @@ class Publishing {
 
                     android.libraryVariants.all { variant ->
                         if (variant.name == 'release') {
-                            owner.classpath += variant.getJavaCompileProvider().classpath
+                            owner.classpath += variant.getJavaCompile().classpath
                         }
                     }
 
